c438028eea4fa68eb3e99a9008cbfad59bfa4219
3li <alisoft239@gmail.com>
2026-01-25 07:31:35 +0200

    AJAX

--- js learn file/api.js
+++ js learn file/api.js
@@ -46,3 +46,155 @@ console.log("1");
 console.log("2");
 setTimeout(() => console.log("Operation"), 3000);
 console.log("3");
+
+
+// -------------------------Call Stack And web api-----174----
+/*
+  To Understand Ajax, Fetch, Promises
+
+  Call Stack || Stack Trace
+  -- JavaScript Engine Uses A Call Stack To Manage Execution Contexts
+  -- Mechanism To Make The Interpreter Track Your Calls
+  -- When Function Called It Added To The Stack
+  -- When Function Executed It Removed From The Stack
+  -- After Function Is Finished Executing The Interpreter Continue From The Last Point
+  -- Work Using LIFO Principle => Last In First Out
+  -- Code Execution Is Synchronous.
+  -- Call Stack Detect Web API Methods And Leave It To The Browser To Handle It
+
+  Web API
+  -- Methods Available From The Environment => Browser
+  ال web api أخر حاجه تطبع بعد ال call stack لان البراوزر هو اللي بيهاندل كل حاجه
+*/
+
+setTimeout(() => {
+  console.log("Web API");
+}, 0);
+
+function one() {
+  console.log("One");
+}
+function two() {
+  one();
+  console.log("Two");
+}
+function three() {
+  two();
+  console.log("Three");
+}
+
+three();
+
+/*
+================================= آخر حاجه تدخل هي اول حاجه تطلع
+console.log("One");
+=================================
+function one() {
+  console.log("One");
+}
+=================================
+function two() {
+  one();
+  console.log("Two");
+}
+=================================
+function three() {
+  two();
+  console.log("Three");
+}
+=================================
+*/
+
+console.log("#####");
+console.log("One");
+console.log("Two");
+console.log("Three");
+
+// -------------------------Event Loop + Callback Queue-----175----
+/*
+  To Understand Ajax, Fetch, Promises
+
+  Event Loop + Callback Queue
+
+  Story
+  - JavaScript Is A Single Threaded Language "All Operations Executed in Single Thread كل العمليات بتتنفذ في خيط واحد"
+  - Call Stack Track All Calls
+  - Every Function Is Done Its Poped Out
+  - When You Call Asynchronous Function It Sent To Browser API
+  - Asynchronous Function Like Settimeout Start Its Own Thread
+  - Browser API Act As A Second Thread
+  - API Finish Waiting And Send Back The Function For Processing
+  - Browser API Add The Callback To Callback Queue
+  - Event Loop Wait For Call Stack To Be Empty
+  - Event Loop Get Callback From Callback Queue And Add It To Call Stack
+  - Callback Queue Follow FIFO "First In First Out" Rule
+*/
+
+console.log("One");
+setTimeout(() => {
+  console.log("Three");
+}, 0);
+setTimeout(() => {
+  console.log("Four");
+}, 0);
+console.log("Two");
+
+setTimeout(() => {
+  console.log(myVar);
+}, 0);
+
+let myVar = 100;
+myVar += 100;
+
+// -------------------------AJAX-----176----
+
+
+/*
+  AJAX
+  - Asynchronous JavaScript And XML
+  - Approach To Use Many Technologies Together [HTML, CSS, Js, DOM]
+  - It Use "XMLHttpRequest" Object To Interact With The Server
+  - You Can Fetch Data Or Send Data Without Page Refresh
+  - Examples
+  --- Youtube Studio
+  --- Google Drive
+  --- Upload Article Photo
+  --- Form Check Name
+
+  Test new XMLHttpRequest();
+  Request And Response
+  Status Code
+*/
+
+let req = new XMLHttpRequest();
+console.log(req);
+
+
+// -------------------------Request Real API-----177----
+
+
+/*
+  Ajax
+  - Ready State => Status Of The Request
+  [0] Request Not Initialized
+  [1] Server Connection Established
+  [2] Request Received
+  [3] Processing Request
+  [4] Request Is Finished And Response Is Ready
+  - Status
+  [200] Response Is Successful
+  [404] Not Found
+*/
+
+let myRequest = new XMLHttpRequest();
+myRequest.open("GET", "https://api.github.com/users/elzerowebschool/repos");
+myRequest.send();
+console.log(myRequest);
+
+myRequest.onreadystatechange = function () {
+  // console.log(myRequest.readyState);
+  // console.log(myRequest.status);
+  if (this.readyState === 4 && this.status === 200) {
+    console.log(this.responseText);
+  }
+};
